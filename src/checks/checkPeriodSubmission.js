082c69c7e9458b8367401f7aecd954ab18518096
Kosta Korenkov <kosta@whoot.me>
2019-02-06 13:13:03 +0300

    check period: fail if no period in 35 minutes

--- src/checks/checkPeriodSubmission.js
+++ src/checks/checkPeriodSubmission.js
@@ -9,12 +9,12 @@ module.exports = async (config, provider, db) => {
   const lastPeriod = await bridge.periods(tipHash);
 
   const timeAgo = Math.abs(lastPeriod.timestamp - Date.now() / 1000);
-  const fortyMinutes = 40 * 60;
+  const thirtyFiveMinutes = 35 * 60;
 
-  if (fortyMinutes < timeAgo) {
+  if (thirtyFiveMinutes < timeAgo) {
     if (await notNotifiedRecently(db, 'periodSub')) {
       return `
-        💥 *Last period was submitted more than 30 minutes ago*
+        💥 *Last period was submitted more than 35 minutes ago*
         Height: ${lastPeriod.height}
         Time: ${new Date(lastPeriod.timestamp * 1000)}
         Tip hash: ${tipHash}
